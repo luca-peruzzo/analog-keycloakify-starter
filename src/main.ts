b44813d3de763efd1e035015cc783ac078c04b6c
Luca Peruzzo <luca.peruzzo@euronovate.com>
2024-09-23 10:52:02 +0200

    fix: unsupported await

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import '@angular/compiler';
 
-import { APP_INITIALIZER, ApplicationRef, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { ClassKey } from 'keycloakify/login';
 import { KcContext } from 'keycloakify/login/KcContext';
-import { getI18n, I18N } from './login/i18n';
+import { getI18n, I18n, I18N } from './login/i18n';
 import { CLASSES, KC_CONTEXT, USE_DEFAULT_CSS } from './login/KcContext';
 import { getKcContextMock } from './login/KcContextMock';
 import { I18nService } from './login/services/i18n.service';
@@ -19,16 +19,17 @@ if (import.meta.env.DEV) {
 const classes = { kcBodyClass: 'lp' } satisfies { [key in ClassKey]?: string };
 
 if (!window.kcContext) {
-  const NoContextComponent = (await import('./no-context.component')).NoContextComponent;
-  bootstrapApplication(NoContextComponent);
+  const NoContextComponentPromise = import('./no-context.component').then((c) => c.NoContextComponent);
+  NoContextComponentPromise.then((NoContextComponent) => bootstrapApplication(NoContextComponent));
 } else {
-  let ComponentBootstrap;
+  let ComponentBootstrapPromise;
   let doUseDefaultCss = true;
   const kcContext = window.kcContext;
+  console.log(kcContext.pageId);
   switch (kcContext.pageId) {
     case 'login.ftl':
       doUseDefaultCss = true;
-      ComponentBootstrap = (await import('./login/pages/login/login.component')).LoginComponent;
+      ComponentBootstrapPromise = import('./login/pages/login/login.component').then((c) => c.LoginComponent);
       break;
     // case 'register.ftl':
     // case 'info.ftl':
@@ -41,8 +42,9 @@ if (!window.kcContext) {
     // case 'login-otp.ftl':
     case 'login-username.ftl':
       doUseDefaultCss = true;
-      ComponentBootstrap = (await import('./login/pages/login-username/login-username.component'))
-        .LoginUsernameComponent;
+      ComponentBootstrapPromise = import('./login/pages/login-username/login-username.component').then(
+        (c) => c.LoginUsernameComponent,
+      );
       break;
     // case 'webauthn-authenticate.ftl':
     // case 'webauthn-register.ftl':
@@ -72,51 +74,58 @@ if (!window.kcContext) {
     //   break;
     default:
       doUseDefaultCss = false;
-      ComponentBootstrap = (await import('./no-context.component')).NoContextComponent;
+      ComponentBootstrapPromise = import('./no-context.component').then((c) => c.NoContextComponent);
       break;
   }
-  if (ComponentBootstrap) {
-    const appRef: ApplicationRef = await bootstrapApplication(ComponentBootstrap, {
-      providers: [
-        provideExperimentalZonelessChangeDetection(),
-        {
-          provide: KC_CONTEXT,
-          useValue: window.kcContext,
-        },
-        {
-          provide: APP_INITIALIZER,
-          multi: true,
-          useFactory: (i18nService: I18nService, kcContext: KcContext) => async () => {
-            const { i18n: _i18n, prI18n_currentLanguage } = getI18n({
-              kcContext,
-            });
-            let i18n = _i18n;
-            if (prI18n_currentLanguage) {
-              i18n = await prI18n_currentLanguage;
-            }
-            i18nService.i18n = i18n;
-            return true;
+  if (ComponentBootstrapPromise) {
+    console.log('bootstrap');
+    ComponentBootstrapPromise.then((ComponentBootstrap) => {
+      bootstrapApplication(ComponentBootstrap, {
+        providers: [
+          provideExperimentalZonelessChangeDetection(),
+          {
+            provide: KC_CONTEXT,
+            useValue: window.kcContext,
           },
-          deps: [I18nService, KC_CONTEXT],
-        },
-        { provide: USE_DEFAULT_CSS, useValue: doUseDefaultCss },
-        { provide: CLASSES, useValue: classes },
-        {
-          provide: I18N,
-          useFactory: (i18nService: I18nService) => {
-            return i18nService.i18n;
+          {
+            provide: APP_INITIALIZER,
+            multi: true,
+            useFactory: (i18nService: I18nService, kcContext: KcContext) => async () => {
+              const { i18n, prI18n_currentLanguage } = getI18n({
+                kcContext,
+              });
+              let i18nPromise = new Promise<I18n>((resolve) => resolve(i18n));
+              if (prI18n_currentLanguage) {
+                i18nPromise = prI18n_currentLanguage;
+              }
+              return i18nPromise.then((i18n) => {
+                i18nService.i18n = i18n;
+                return true;
+              });
+            },
+            deps: [I18nService, KC_CONTEXT],
           },
-          deps: [I18nService],
-        },
-      ],
-    });
-    appRef.components.forEach((componentRef) => {
-      if ('classes' in componentRef.instance) {
-        componentRef.setInput('classes', classes);
-      }
-      if ('doUseDefaultCss' in componentRef.instance) {
-        componentRef.setInput('doUseDefaultCss', doUseDefaultCss);
-      }
+          { provide: USE_DEFAULT_CSS, useValue: doUseDefaultCss },
+          { provide: CLASSES, useValue: classes },
+          {
+            provide: I18N,
+            useFactory: (i18nService: I18nService) => {
+              return i18nService.i18n;
+            },
+            deps: [I18nService],
+          },
+        ],
+      }).then((appRef) => {
+        console.log('appRef: ', appRef);
+        appRef.components.forEach((componentRef) => {
+          if ('classes' in componentRef.instance) {
+            componentRef.setInput('classes', classes);
+          }
+          if ('doUseDefaultCss' in componentRef.instance) {
+            componentRef.setInput('doUseDefaultCss', doUseDefaultCss);
+          }
+        });
+      });
     });
   }
 }
